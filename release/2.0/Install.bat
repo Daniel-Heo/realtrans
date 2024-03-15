4e2e43ccfc1f4c710afde4727ae16aa76a3770c8
Daniel-Heo <160463127+Daniel-Heo@users.noreply.github.com>
2024-03-15 18:45:06 +0900

    Update Install.bat

--- release/2.0/Install.bat
+++ release/2.0/Install.bat
@@ -1,15 +1,5 @@
 @echo off
 
-if not exist "%~dp0\venv\Scripts" (
-    echo Creating venv...
-    python -m venv venv
-)
-
-echo checked the venv folder. now installing requirements..
-cd /d "%~dp0\venv\Scripts"
-call activate.bat
-
-cd /d "%~dp0"
 pip install -r requirements.txt
 
 if errorlevel 1 (
