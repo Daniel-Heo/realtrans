6d9332d2def8f1326d7fae2af74ec85cea306d29
Daniel-Heo <160463127+Daniel-Heo@users.noreply.github.com>
2025-02-13 18:42:32 +0900

    Fix duplicate resource file numbers
    Fix duplicate resource file numbers

--- src/resource.h
+++ src/resource.h
@@ -1,21 +1,31 @@
-﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++에서 생성한 포함 파일입니다.
-// RealTrans.rc에서 사용되고 있습니다.
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 
 //
+#define _APS_NO_MFC                     1
+#define IDR_MENU1                       2
 #define IDINIT                          3
+#define IDS_MODEL_CHANGE_WARNING        4
+#define IDM_EXIT                        5
 #define IDD_REALTRANS_DIALOG            102
 #define IDS_APP_TITLE                   103
 #define IDM_ABOUT                       104
-#define IDS_MODEL_CHANGE_WARNING        104
-#define IDM_EXIT                        105
 #define IDS_ALERT                       105
 #define IDS_TRANSLATE                   106
 #define IDI_SMALL                       108
 #define IDC_REALTRANS                   109
+#define IDC_TEXT_MODEL_SIZE             110
+#define IDC_TEXT_MODEL_SIZE2            111
+#define IDC_TEXT_MODEL_SIZE3            112
+#define IDC_TEXT_SEL_VLANG21            113
+#define IDC_COMBO_TRANSPARENT           114
+#define IDC_TEXT_SEL_VLANG20            115
+#define IDC_BG_COLOR                    116
+#define IDC_INIT                        117
+#define IDC_COMBO_OLDFONT_COLOR         118
 #define IDI_REALTRANS                   127
 #define IDR_MAINFRAME                   128
 #define IDI_SETTINGS                    132
-#define IDR_MENU1                       132
 #define IDB_BITMAP_MENU_N               133
 #define IDB_BITMAP_MENU_O               134
 #define IDD_DIALOG_SETUP                136
@@ -29,23 +39,19 @@
 #define IDC_COMBO_PCTRANS_LANG          1006
 #define IDC_TEXT_SEL_VLANG2             1007
 #define IDC_COMBO_VOICE_LANG2           1008
-#define IDC_TEXT_MODEL_SIZE             1008
 #define IDC_CHECK_VSUB2                 1009
-#define IDC_TEXT_MODEL_SIZE2            1009
 #define IDC_CHECK_PCTRANS               1010
 #define IDC_TEXT_SEL_VLANG5             1011
 #define IDC_TEXT_SEL_VLANG6             1012
 #define IDC_CHECK_APITRANS              1013
 #define IDC_TEXT_SEL_VLANG7             1014
 #define IDC_COMBO_FONT_COLOR            1015
-#define IDC_TEXT_MODEL_SIZE3            1015
 #define IDC_TEXT_SEL_VLANG8             1016
 #define IDC_TEXT_SEL_VLANG18            1017
 #define IDC_TEXT_SEL_VLANG12            1018
 #define IDC_COMBO_APITRANS_LANG         1019
 #define IDC_TEXT_SEL_VLANG9             1020
 #define IDC_COMBO_API                   1021
-#define IDC_TEXT_SEL_VLANG21            1021
 #define IDC_TEXT_SEL_VLANG10            1022
 #define IDC_EDIT2                       1023
 #define IDC_TEXT_SEL_VLANG11            1024
@@ -55,12 +61,10 @@
 #define IDC_COMBO_SUM_API               1030
 #define IDC_TEXT_SEL_VLANG14            1031
 #define IDC_EDIT3                       1032
-#define IDC_COMBO_TRANSPARENT           1032
 #define IDC_TEXT_SEL_VLANG15            1033
 #define IDC_EDIT_FONT_SIZE              1034
 #define IDC_EDIT_OLDFONT_SIZE           1035
 #define IDC_EDIT_TRANS_API_SEC          1036
-#define IDC_TEXT_SEL_VLANG20            1036
 #define IDC_EDIT_SUMFONT_SIZE           1037
 #define IDC_TEXT_SEL_VLANG16            1038
 #define IDC_EDIT_SUMMARY_HINT           1039
@@ -72,15 +76,12 @@
 #define IDC_EDIT_SUMFONT_COLOR          1045
 #define IDC_TEXT_SEL_VLANG19            1046
 #define ID_INIT                         1047
-#define IDC_INIT                        1048
 #define IDC_EDIT_SUMFONT_COLOR2         1048
-#define IDC_BG_COLOR                    1048
 #define IDC_SYSLINK1                    1049
 #define IDC_COMBO_UI_LANG               1050
 #define IDC_COMBO_TRANS_API             1051
 #define IDC_CHECK_SUMMARY               1052
 #define IDC_TEXT_SEL_VLANG17            1053
-#define IDC_COMBO_OLDFONT_COLOR         1054
 #define IDC_COMBO_MODEL_SIZE            1054
 #define IDC_EDIT_TRANS_API_KEY          1055
 #define IDC_EDIT_SUMMARY_API_KEY        1056
@@ -93,16 +94,15 @@
 #define IDC_STATIC_ARROW                1064
 #define IDC_BTN_OPEN_FILE               1065
 #define IDC_BTN_SAVE_FILE               1066
-#define IDC_STATIC                      -1
+#define IDC_STATIC                      4294967295
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        140
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1067
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           1159
 #endif
 #endif
